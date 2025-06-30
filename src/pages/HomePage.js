f62b28059fc029cdb7eaccf3d8a6041990ff9bd3
Pranav Nag B <85959630+pranavgnr@users.noreply.github.com>
2025-06-30 08:45:38 +0530

    Fix jsx attribute error in HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -246,7 +246,7 @@ const HomePage = () => {
         )}
       </Container>
 
-      <style jsx>{`
+      <style>{`
         @keyframes gradient {
           0% { background-position: 0% 50%; }
           50% { background-position: 100% 50%; }
